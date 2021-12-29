7f7ea041614b97794907b70250d07c2af6fcbbfe
Ravikumar <96512246+ravikumar0112@users.noreply.github.com>
2021-12-29 13:52:11 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,13 @@
+from tkinter import*
+import time
+
+def digital ():
+    d=time.strftime("%H:%M:%S")
+    clock.config(text=d)
+    clock.after(200,digital)
+root=Tk()
+root.title("digital clock")
+clock=Label(root,font=("times",100,"bold"),bg="pink")
+clock.grid(row=2,column=1)
+digital()
+root.mainloop()
